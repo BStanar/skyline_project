d96f7cf5ff62ad297c86190f0e706afc07a4d38d
BSSarajevo <bstanar96@gmail.com>
2024-01-30 15:57:56 +0100

    Add project files.

---
+++ Osoba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skyline_project
+{
+    internal interface Osoba
+    {
+        string ime { get; set; }
+        string prezime {  get; set; }
+        DateTime? datumRodjenja { get; set; }
+        int broj_telefona { get; set; }
+
+    }
+}
