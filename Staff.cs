d10f89b37ba1cbeb36efcda6e0658d59cb067e41
BSSarajevo <bstanar96@gmail.com>
2024-01-31 16:42:25 +0100

    Test

---
+++ Staff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skyline_project
+{
+    internal class Staff : IStaff
+    {
+        public string StaffID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Department { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime DateOfJointing { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public decimal Salary { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string lastname { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? dateOfBirth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int telephoneNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AssignDepartment(string department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetStaffDetail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSalary(decimal Salary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
